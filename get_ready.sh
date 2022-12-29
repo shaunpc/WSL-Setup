aac1025f3f24af0f53ef96a7332182e9d0bbdc69
Shaun PC <shauncotter00@gmail.com>
2022-12-29 17:19:32 +0000

    Update get_ready.sh

--- get_ready.sh
+++ get_ready.sh
@@ -1,4 +1,11 @@
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade
+sudo get update
+sudo get upgrade -y
+sudo apt dist-upgrade
 do-release-upgrade
+
+sudo apt get screenfetch
+echo "" >> .profile
+echo "# Display pretty machine and login details" >> .profile
+echo "echo" >> .profile
+echo "screenfetch" >> .profile
+echo "echo" >> .profile
