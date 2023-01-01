8787873372f533c13a9e7be14c92fa041e40df1f
Shaun PC <shauncotter00@gmail.com>
2023-01-01 17:52:22 +0000

    vscode optimisation

--- get_ready.sh
+++ get_ready.sh
@@ -26,10 +26,10 @@ echo "echo" >> .profile
 # Ensure VS Code installs itself (if left to end, the 'code' command not recognised)
 let STEP++
 echo -e '\n' $BBlue $(date +"%T") $Green "Step $STEP >> Checking VS Code install \n" $Color_Off
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+sudo apt install code -y
 
 # Setup GIT
 let STEP++
